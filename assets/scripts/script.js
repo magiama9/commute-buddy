599cffbe31bb4c856f4fcdfe79c2bf5d11d5ad2c
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-01-14 14:56:32 -0500

    Add basic mapGL rendering and direction control

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,18 @@
-$.ajax({
-  url: uvURL,
-  method: "GET",
-  data: {
-    lat: currentLat,
-    lon: currentLon,
-    APPID: "968b7fae60d97ecc132a2221d027b935"
-  },
-  success: function(response) {
-    currentUV = response.value;
-    console.log(currentUV);
-    displayUV();
-  },
-  error: function() {
-    console.log("ERROR UV");
-  }
+$(document).ready(function() {
+  let directionsURL = "https://api.mapbox.com/directions/v5/mapbox/driving/-77%2C39%3B-77%2C38.895.json?";
+
+  $.ajax({
+    url: directionsURL,
+    method: "GET",
+    data: {
+      access_token:
+        "pk.eyJ1IjoibWFnaWFtYTkiLCJhIjoiY2s1ZTQ5eTd0MDBmMjNsbm00dTQ4cWcydCJ9.Fsow7km2bw_bHw0r7jtPlw"
+    },
+    success: function(response) {
+      console.log(response);
+    },
+    error: function() {
+      console.log("ERROR");
+    }
+  });
 });
